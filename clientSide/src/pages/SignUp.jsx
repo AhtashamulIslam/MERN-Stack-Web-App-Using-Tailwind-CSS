82dbb322123a3fd962719f00ef7313e2c496409e
AhtashamulIslam Shobuj <ehsanbdstore@gmail.com>
2024-06-29 16:04:35 +0600

    add functioanlity to the sign up page

--- clientSide/src/pages/SignUp.jsx
+++ clientSide/src/pages/SignUp.jsx
@@ -1,8 +1,43 @@
-import { Label, TextInput , Button } from 'flowbite-react'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Label, TextInput , Button, Alert, Spinner } from 'flowbite-react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 function SignUp() {
+   const [formData,setFormData]=useState({})
+   const [errorMessage,setErrorMessage]=useState(null)
+   const [loading,setLoading]=useState(false)
+   const navigate=useNavigate()
+   
+   const handleChange = (e)=>{
+      setFormData({...formData,[e.target.id]:e.target.value.trim()})
+      //Here each time the field data will be destructed which will be stored in fromData as a single valued object. And every time the field data be filled via id and add to the destructured properties.
+   }
+   console.log(formData)
+   const handleSubmit = async (e)=>{
+    e.preventDefault()
+    if(!formData.username || !formData.email || !formData.password)
+       return setErrorMessage('Please fill out all fields')
+    try{
+        setLoading(true)
+        setErrorMessage(null)// Set them to update the previous states
+        const res = await fetch(`/api/auth/signup`,{
+          method:'POST',
+          headers: {'Content-Type':'application/json'},
+          body:JSON.stringify(formData)
+        })
+        const data = await res.json()
+        //Contains the response status like success and message
+        if(data.success===false)
+          return setErrorMessage(data.message)
+        setLoading(false)
+        if(res.ok)
+          navigate('/signin')
+    }catch(error){
+      //Handling from client side.
+      setErrorMessage(error.message)
+      setLoading(false)
+    }
+   }
   return (
     <div className='min-h-screen mt-20 '>
       <div className='flex flex-col px-8 sm:px-12  max-w-3xl mx-auto md:flex-row md:p-3 md:items-center gap-5 '>
@@ -14,21 +49,28 @@ function SignUp() {
         <p className='text-sm mt-5'>This is a personal project. You can sign up with your email and password or with Google</p>
         </div>
         <div className='flex-1'>
-          <form className='flex flex-col gap-4'>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
            <div className='space-y-1'>
           <Label value='Your username'/>
-          <TextInput type='text' placeholder='Username' id='username' />
+          <TextInput type='text' placeholder='Username' id='username' onChange={handleChange}/>
            </div>
            <div className='space-y-1'>
           <Label value='Your email'/>
-          <TextInput type='text' placeholder='name@company.com' id='email' />
+          <TextInput type='email' placeholder='name@company.com' id='email' onChange={handleChange}/>
            </div>
            <div className='space-y-1'>
           <Label value='Your password'/>
-          <TextInput type='text' placeholder='Password' id='password' />
+          <TextInput type='password' placeholder='Password' id='password' onChange={handleChange}/>
            </div>
-           <Button gradientDuoTone='purpleToPink' type='submit'>
-            Sign Up
+           <Button gradientDuoTone='purpleToPink' type='submit' disabled={loading}>
+            {
+              loading ? (
+                <>
+                <Spinner size='sm'/>
+                <span className='pl-3'>Loading...</span>
+                </>
+              ) : 'Sign Up'
+            }
            </Button>
           </form>
           <div className='flex gap-2 mt-5 text-sm'>
@@ -37,6 +79,12 @@ function SignUp() {
             Sign In
             </Link>
           </div>
+            { errorMessage && (
+             <Alert className='mt-5' color='failure'>
+              {errorMessage}
+             </Alert>
+            )
+          }
         </div>
       </div>
     </div>
--- clientSide/vite.config.js
+++ clientSide/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    proxy:{
+      '/api':{
+        target:'http://localhost:3000',
+         //Change the target to this link when it found /api 
+         secure:false
+      }
+    }
+  },
   plugins: [react()],
 })
